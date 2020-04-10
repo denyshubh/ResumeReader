3f010b9c462cd6bfc3c4c4c36e1b7a22d381f644
denyshubh <denyshubham@gmail.com>
2020-04-10 21:43:43 +0530

    version not yet tetsed

--- server.js
+++ server.js
@@ -14,18 +14,22 @@ aws.config.getCredentials(function (err) {
   }
 });
 aws.config.update({ region: 'us-east-1' });
-let s3 = new aws.S3({ apiVersion: '2006-03-01' });
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+const dynamodb = new aws.DynamoDB();
+const outTable = 'awscdk-OutputTable875D8E18-1NM637OBRWUKY'
+const docTable = 'awscdk-DocumentsTable7E808EE5-1REOV6ARFO9LQ'
+const BUCKET = 'awscdk-documentsbucket9ec9deb9-1u4iiltwuvyj4'
 app.use(bodyParser.json());
 app.use(cors())
-
+let FILE_NAME = "";
 const upload = multer({
   storage: multerS3({
     s3: s3,
     acl: 'public-read',
-    bucket: 'awscdk-documentsbucket9ec9deb9-1u4iiltwuvyj4',
+    bucket: BUCKET,
     key: function (req, file, cb) {
-      console.log(file);
-      cb(null, Date.now() + '-' + file.originalname);
+      FILE_NAME = file.originalname
+      cb(null, FILE_NAME);
     }
   })
 }).array('file');
@@ -45,6 +49,114 @@ app.post('/upload', function (req, res) {
     return res.status(200).send(req.file)
     // Everything went fine.
   })
+});
+
+const getResult = async (file) => {
+  let params = {
+    ExpressionAttributeNames: {
+      "#ON": "objectName",
+      "#ID": "documentId",
+      "#STATUS": "documentStatus"
+    },
+    ExpressionAttributeValues: {
+      ":filename": {
+        S: file
+      }
+    },
+    FilterExpression: "objectName = :filename",
+    ProjectionExpression: "#ON, #ID, #STATUS",
+    TableName: docTable,
+  }
+  let data = dynamodb.scan(params)
+  let res = await data.promise()
+  return res['Items']
+}
+
+const getS3LocationOfAnalysedFile = async (documentId) => {
+  let params = {
+    ExpressionAttributeNames: {
+      "#OP": "outputPath",
+      "#ID": "documentId",
+      "#OT": "outputType"
+    },
+    ExpressionAttributeValues: {
+      ":id": {
+        S: documentId
+      }
+    },
+    KeyConditionExpression: "documentId = :id",
+    ProjectionExpression: "#OP, #ID, #OT",
+    TableName: outTable,
+  }
+  let data = dynamodb.query(params)
+  let res = await data.promise()
+  return res['Items']
+}
+const getSignedUrl = (file) => {
+  const signedUrlExpireSeconds = 60 * 5 // your expiry time in seconds.
+  const url = s3.getSignedUrl('getObject', {
+    Bucket: BUCKET,
+    Key: file,
+    Expires: signedUrlExpireSeconds
+  })
+  return url;
+}
+
+app.get('/download', async (req, res) => {
+  try {
+    console.log(`File received is ${req.query}`)
+    let file = req.query['fileName'][0]
+    let data = await getResult(file);
+    if (!data) {
+      console.log('NO DATA FOUND IN DOCUMENTS TABLE Means Data is not uploaded to s3 or there is problem in the infrastructure. Need human validation');
+      return res.status(404).json({ message: 'Either You have not uploaded your resume or there is network issue. Please try again' })
+    }
+    data = data[0]
+    if (data['documentStatus']['S'] === 'SUCCEEDED') {
+      let files = await getS3LocationOfAnalysedFile(data['documentId']['S'])
+      if (!files) {
+        console.log('No such document exists in output table.')
+        return res.status(500).json({ message: 'Internal Server Error. Please contact the service provider.' })
+      } else {
+        let signedURLS = []
+        for (let i = 0; i < files.length; i++) {
+          let file = files[i]['outputType'].split('');
+          switch (file[2]) {
+            case 'Text':
+              console.log('do something with Text File')
+              break;
+            case 'TextInReadingOrder':
+              console.log('do something with TextInreadingOrder File')
+              break;
+            case 'EntityText':
+              // console.log('do something with Entity File')
+              let url = getSignedUrl(files[i]['outputPath'])
+              signedURLS.push(url)
+              break;
+            case 'Forms':
+              console.log('do something with Forms')
+              break;
+            case 'Response':
+              console.log('do something with Textract Response')
+              break;
+            case 'Tables':
+              console.log('do something with Tables')
+              break;
+            default:
+              console.log('Something Went Wrong!!!')
+
+          }
+        }
+        return res.status(200).send({ urls: signedURLS })
+      }
+    } else {
+      console.log('Data is still in process state')
+      return res.status(404).json({ message: 'Data is still being processed. Please try again after few seconds.' })
+    }
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: 'Please Upload a File' })
+  }
 });
 
 app.listen(8000, function () {
--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ class App extends Component {
     super(props);
     this.state = {
       selectedFile: null,
-      loaded: 0
+      loaded: 0,
+      download: 0,
+      disableUploadButton: false,
+      disableDownloadButton: true
     }
 
   }
@@ -20,14 +23,14 @@ class App extends Component {
     // list allow mime type
     const types = ['application/pdf', 'application/msword', 'text/plain']
     // loop access array
-    for (var x = 0; x < files.length; x++) {
+    for (let x = 0; x < files.length; x++) {
       // compare file type find doesn't matach
       if (types.every(type => files[x].type !== type)) {
         // create error message and assign to container   
         err[x] = files[x].type + ' is not a supported format\n';
       }
     };
-    for (var z = 0; z < err.length; z++) {// if message not same old that mean has error 
+    for (let z = 0; z < err.length; z++) {// if message not same old that mean has error 
       // discard selected file
       toast.error(err[z])
       event.target.value = null
@@ -48,7 +51,7 @@ class App extends Component {
     let files = event.target.files
     let size = 2000000
     let err = [];
-    for (var x = 0; x < files.length; x++) {
+    for (let x = 0; x < files.length; x++) {
       if (files[x].size > size) {
         err[x] = files[x].type + 'is too large, please pick a smaller file\n';
       }
@@ -61,41 +64,90 @@ class App extends Component {
     return true;
   }
   onChangeHandler = event => {
-    var files = event.target.files
+    let files = event.target.files
     console.log(files)
     if (this.maxSelectFile(event) && this.checkMimeType(event) && this.checkFileSize(event)) {
       // if return true allow to setState
       this.setState({
         selectedFile: files,
-        loaded: 0
+        loaded: 0,
       })
     }
   }
   onClickHandler = () => {
-    const data = new FormData()
-    for (var x = 0; x < this.state.selectedFile.length; x++) {
-      data.append('file', this.state.selectedFile[x])
-    }
-    axios.post("http://localhost:8000/upload", data, {
-      onUploadProgress: ProgressEvent => {
-        this.setState({
-          loaded: (ProgressEvent.loaded / ProgressEvent.total * 100),
+    try {
+      const data = new FormData()
+      for (let x = 0; x < this.state.selectedFile.length; x++) {
+        let file = Date.now() + '-' + this.state.selectedFile[x]
+        data.append('file', file)
+      }
+      axios.post("http://localhost:8000/upload", data, {
+        onUploadProgress: ProgressEvent => {
+          this.setState({
+            loaded: (ProgressEvent.loaded / ProgressEvent.total * 100),
+          })
+        },
+      })
+        .then(res => { // then print response status
+          toast.success('Upload Success!')
         })
-      },
-    })
-      .then(res => { // then print response status
-        toast.success('upload success')
+        .catch(err => { // then print response status
+          toast.error('Upload Failed!')
+        })
+      this.setState({
+        disableDownloadButton: false
       })
-      .catch(err => { // then print response status
-        toast.error('upload fail')
-      })
-    console.log(data)
-  }
+    } catch (err) {
+      toast.warn('Please Upload Your Resume')
+    }
 
+  }
+  onDownloadClick = () => {
+    this.setState({
+      disableUploadButton: true,
+      disableDownloadButton: true
+    })
+    let selectedFile = this.state.selectedFile
+    try {
+      axios.get("http://localhost:8000/download", {
+        params: {
+          fileName: selectedFile
+        }
+      })
+        .then(res => { // then print response status
+          axios.get(res.urls[0], {
+            onDownloadProgress: ProgressEvent => {
+              this.setState({
+                download: (ProgressEvent.loaded / ProgressEvent.total * 100),
+              })
+            },
+          })
+            .then(res => { // then print response status
+              toast.success('Download Complete!')
+            })
+            .catch(err => { // then print response status
+              toast.error('Download Failed')
+            })
+        })
+        .catch(err => { // then print response status
+          console.log(err)
+          toast.error('Download Failed')
+        })
+
+      this.setState({
+        disableUploadButton: false
+      })
+    } catch (e) {
+      console.log('Please Upload The File First!!!')
+    }
+
+  }
   render() {
+    let { loaded, download, disableUploadButton, disableDownloadButton } = this.state
+    console.log(disableDownloadButton, disableUploadButton, loaded, download)
     return (
       <div className="container">
-        <div className="row">
+        <div className="row jumbotron">
           <div className="offset-md-3 col-md-6">
             <div className="form-group files">
               <label>Upload Your File </label>
@@ -103,12 +155,19 @@ class App extends Component {
             </div>
             <div className="form-group">
               <ToastContainer />
-              <Progress max="100" color="success" value={this.state.loaded} >{Math.round(this.state.loaded, 2)}%</Progress>
+              <Progress max="100" color="success" value={loaded} >{Math.round(loaded, 2)}%</Progress>
 
             </div>
 
-            <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
+            <button type="button" disabled={disableUploadButton} className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
 
+          </div>
+        </div>
+        <div className="row">
+          <div className="offset-md-3 col-md-6">
+            <ToastContainer />
+            <button type="button" disabled={disableDownloadButton} className="btn btn-primary btn-block" onClick={this.onDownloadClick}>Download</button>
+            <Progress max="100" color="info" value={download} >{Math.round(download, 2)}%</Progress>
           </div>
         </div>
       </div>
